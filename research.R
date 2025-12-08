49fc02e7b1d719acb4c0a43e42bad8f87ff0c14a
mt25aax <mt25aax@herts.ac.uk>
2025-12-08 18:22:42 +0500

    load Data

--- research.R
+++ research.R
@@ -1,3 +1,10 @@
 # Clear environment and set options
 rm(list = ls())
 options(scipen = 999, digits = 10)
+
+# Create output directory
+if (!dir.exists("output")) dir.create("output")
+
+# Load data
+helium_data <- read.csv("user_input_files/Helium.csv", stringsAsFactors = FALSE)
+helium_data$Date <- as.Date(helium_data$Date, format = "%Y-%m-%d")
